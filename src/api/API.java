773f4fcc9fa6cbeeda3c9b1aee073e334a0c5681
CmD0 <elouan112@gmail.com>
2023-04-19 18:53:49 -0400

    api repo creation

---
+++ src/api/API.java
@@ -0,0 +1,7 @@
+package api;
+
+public class API {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
---
+++ src/test/java/com/cmd0app/boardlisapi/BoardlisApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmd0app.boardlisapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardlisApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
